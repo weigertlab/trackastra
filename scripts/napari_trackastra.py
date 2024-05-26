882572493d260ec5564f6441ff3a24c7761a7b5b
Martin <marweigert@gmail.com>
2024-05-27 01:22:23 +0200

    bump napari demo

--- scripts/napari_trackastra.py
+++ scripts/napari_trackastra.py
@@ -15,22 +15,33 @@ from trackastra.tracking import graph_to_ctc, graph_to_napari_tracks
  
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
+def track(model, imgs, masks, **kwargs):    
+    imgs = np.stack([normalize(x) for x in imgs])
+    
+    
+    track_graph = model.track(imgs, masks, mode="greedy", 
+                                max_distance=128,
+                                progbar_class=progress,
+                                **kwargs)  # or mode="ilp"
+    # Visualise in napari
+    df, masks_tracked = graph_to_ctc(track_graph,masks,outdir=None)
+    napari_tracks, napari_tracks_graph, _ = graph_to_napari_tracks(track_graph)
+    return track_graph, masks_tracked, napari_tracks
+
+
 def create_widget(model_path:Path):
-    @magicgui(call_button="track", model_path={"label": "Model Path", "mode": "d"})
-    def my_widget(imgs: Image, masks:Labels, model_path:Path=model_path) -> List[napari.types.LayerDataTuple]:
-        img = np.asarray(imgs.data)
-        mask = np.asarray(masks.data)
-        imgs = np.stack([normalize(x) for x in img])
-
-        model = Trackastra.load_from_folder(model_path, device=device)
-        
-        track_graph = model.track(img, mask, mode="greedy", progbar_class=progress)  # or mode="ilp"
-
-        # Visualise in napari
-        napari_tracks, napari_tracks_graph, _ = graph_to_napari_tracks(track_graph)
-        _, masks_tracked = graph_to_ctc(track_graph,mask,outdir=None)
-        masks.visible = False
-        return [(napari_tracks, dict(name='tracks',tail_length=10), "tracks"), (masks_tracked, dict(name='masks_tracked', opacity=0.3), "labels")]
+    @magicgui(call_button="track", 
+              model_path={"label": "Model Path", "mode": "d"})
+    def my_widget(img_layer: Image, mask_layer:Labels, model_path:Path=model_path, distance_costs:bool=False) -> List[napari.types.LayerDataTuple]:
+        if model_path.exists():
+            model = Trackastra.load_from_folder(model_path, device=device)
+        else: 
+            model = Trackastra.load_pretrained(model_path.name, device=device)
+        imgs = np.asarray(img_layer.data)
+        masks = np.asarray(mask_layer.data)
+        track_graph, masks_tracked, napari_tracks = track(model, imgs, masks, use_distance=distance_costs)
+        mask_layer.visible = False
+        return [(napari_tracks, dict(name='tracks',tail_length=5), "tracks"), (masks_tracked, dict(name='masks_tracked', opacity=0.3), "labels")]
     
     return my_widget
 
@@ -40,6 +51,7 @@ if __name__ == "__main__":
     parser.add_argument("-i", "--img", type=str, default=None) 
     parser.add_argument("-m", "--mask", type=str, default=None) 
     parser.add_argument("--model", type=str, default=None)
+    parser.add_argument("--debug", action="store_true")
     
     args=parser.parse_args()
 
@@ -62,6 +74,11 @@ if __name__ == "__main__":
         model_path = Path(args.model)
     else: 
         model_path = None
-    viewer.window.add_dock_widget(create_widget(args.model))
+        
+    if args.debug:
+        model = Trackastra.load_from_folder(model_path, device=device)
+        track_graph, masks_tracked, napari_tracks = track(model, imgs, masks)
+    else:
+        viewer.window.add_dock_widget(create_widget(args.model))
 
     napari.run()
