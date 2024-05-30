7fedd403ec56e90faafab4bd2f5fb55dbf67582c
Benjamin Gallusser <bgallusser@googlemail.com>
2024-05-30 11:27:29 +0200

    Limit ILP tracking to <=py311

--- trackastra/tracking/ilp.py
+++ trackastra/tracking/ilp.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 import time
 from types import SimpleNamespace
 
@@ -7,6 +8,9 @@ import yaml
 
 try:
     import motile
+
+    if sys.version_info >= (3, 12):
+        raise ImportError("The optional dependency 'motile' requires Python <3.12.")
 except ModuleNotFoundError:
     raise ModuleNotFoundError(
         "For tracking with an ILP, please conda install the optional `motile`"
