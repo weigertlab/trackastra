315e4b49375e2e95d0fee2652ec71d5dd09e15e5
Benjamin Gallusser <bgallusser@googlemail.com>
2024-06-17 17:28:23 +0200

    Update tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -15,9 +15,9 @@ def test_cli_tracking_from_folder():
     cmd = "trackastra track -i test_data/img -m test_data/TRA --output-ctc test_data/tracked --output-edge-table test_data/tracked.csv --model-pretrained general_2d"  # noqa: RUF100
     print(cmd)
     result = shell(cmd)
-    assert result.code == 0
     assert Path("test_data/tracked").exists()
     assert Path("test_data/tracked.csv").exists()
+    assert result.code == 0
 
 
 def test_cli_tracking_from_file():
@@ -27,6 +27,6 @@ def test_cli_tracking_from_file():
     cmd = f"trackastra track -i {root / 'trpL_150310-11_img.tif'} -m {root / 'trpL_150310-11_mask.tif'} --output-ctc {output_ctc} --output-edge-table {output_edge_table} --model-pretrained general_2d"  # noqa: RUF100
     print(cmd)
     result = shell(cmd)
-    assert result.code == 0
     assert output_ctc.exists()
     assert output_edge_table.exists()
+    assert result.code == 0
