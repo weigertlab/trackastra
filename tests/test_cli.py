2918a7b0063129c821603b1edcc05244cecfbe1e
Benjamin Gallusser <bgallusser@googlemail.com>
2024-07-02 15:03:22 +0200

    Fix tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -11,7 +11,7 @@ def test_cli_parser():
 
 def test_cli_tracking_from_folder():
     example_dataset()
-    cmd = "trackastra track -i test_data/img -m test_data/TRA --output-ctc test_data/tracked --output-edge-table test_data/tracked.csv --model-pretrained general_2d"  # noqa: RUF100
+    cmd = "trackastra track -i test_data/img -m test_data/TRA --output-ctc test_data/tracked --output-edge-table test_data/tracked.csv --model-pretrained general_2d --device cpu"  # noqa: RUF100
     print(cmd)
     result = os.system(cmd)
     assert Path("test_data/tracked").exists()
@@ -26,7 +26,7 @@ def test_cli_tracking_from_file():
     output_ctc = Path("test_data") / "tracked_bacteria"
     output_edge_table = Path("test_data") / "tracked_bacteria.csv"
 
-    cmd = f"trackastra track -i {root / 'trpL_150310-11_img.tif'} -m {root / 'trpL_150310-11_mask.tif'} --output-ctc {output_ctc} --output-edge-table {output_edge_table} --model-pretrained general_2d"  # noqa: RUF100
+    cmd = f"trackastra track -i {root / 'trpL_150310-11_img.tif'} -m {root / 'trpL_150310-11_mask.tif'} --output-ctc {output_ctc} --output-edge-table {output_edge_table} --model-pretrained general_2d --device cpu"  # noqa: RUF100
     print(cmd)
     result = os.system(cmd)
 
