e076f3c8d9301de39082239969c47bccb6aeb58d
Benjamin Gallusser <bgallusser@googlemail.com>
2025-05-12 12:43:55 -0700

    Add workaround for OMP import error

--- trackastra/__init__.py
+++ trackastra/__init__.py
@@ -3,3 +3,5 @@ import os
 from ._version import __version__, __version_tuple__
 
 os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
+# Workaround for OMP: Error #15: Initializing libomp.dylib, but found libomp.dylib already initialized.
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
